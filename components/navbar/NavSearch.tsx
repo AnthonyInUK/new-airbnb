0a79e1f525c4e1b5c8b3a4b0725b6d34ebe3bc5a
AnthonyInUK <anthonyxiamen@gmail.com>
2024-10-28 01:04:13 -0700

    initial commit

---
+++ components/navbar/NavSearch.tsx
@@ -0,0 +1,8 @@
+import { Input } from "../ui/input"
+
+function NavSearch() {
+    return (
+        <Input className="max-w-xs dark:bg-muted" placeholder="find a property..." type="text" />
+    )
+}
+export default NavSearch
