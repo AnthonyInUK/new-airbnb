2dad2e03c025b1ab2273555670d458d5c6d74dbe
AnthonyInUK <anthonyxiamen@gmail.com>
2024-11-06 13:24:02 -0800

    fix navbar

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -7,7 +7,7 @@ import { redirect } from 'next/navigation';
 
 async function createProfilePage() {
     const user = await currentUser();
-    //if(user?.privateMetadata?.hasProfile) redirect('/')
+    if (user?.privateMetadata?.hasProfile) redirect('/')
     return (<section>
         <h1 className='text-2xl font-semibold mb-8 capitalize'>new user</h1>
         <div className='border p-8 rounded-md'>
