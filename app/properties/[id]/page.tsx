df723be0aa3e440d8727f86f8a348922ead0736e
AnthonyInUK <anthonyxiamen@gmail.com>
2024-11-06 21:01:19 -0800

    a step finish

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { fetchPropertyDetails } from "@/utils/actions"
+import { fetchPropertyDetails, findExistingReview } from "@/utils/actions"
 import { redirect } from 'next/navigation';
 import { Skeleton } from "@/components/ui/Skeleton";
 import { Separator } from "@/components/ui/separator";
@@ -13,6 +13,9 @@ import UserInfo from "@/components/properties/UserInfo";
 import Description from "@/components/properties/Description";
 import Amenities from "@/components/properties/Amenities";
 import dynamic from "next/dynamic";
+import SubmitReview from "@/components/reviews/SubmitReview";
+import PropertyReviews from "@/components/reviews/PropertyReviews";
+import { auth } from '@clerk/nextjs/server';
 
 
 const DynamicMap = dynamic(() => import('@/components/properties/PropertyMap'), {
@@ -27,7 +30,9 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
     const details = { baths, bedrooms, beds, guests }
     const firstName = property.profile.firstName;
     const profileImage = property.profile.profileImage;
-
+    const { userId } = auth();
+    const isNotOwner = property.profile.clerkId !== userId;
+    const reviewDoesNotExist = userId && isNotOwner && !(await findExistingReview(userId, property.id));
     return (
         <section><BreadCrumbs name={property.name} />
             <header className="flex justify-between items-center mt-4"><h1 className='text-4xl font-bold '>{property.tagline}</h1>
@@ -45,12 +50,16 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
                     <UserInfo profile={{ firstName, profileImage }} />
                     <Separator className='mt-4' /><Description description={property.description} />
                     <Amenities amenities={property.amenities} />
-                    <DynamicMap countryCode={property.country} />;
+                    <DynamicMap countryCode={property.country} />
                 </div>
                 <div className='lg:col-span-4 flex flex-col items-center'>
                     <BookingCalendar />
                 </div>
-            </section></section>
+            </section>
+            {reviewDoesNotExist && <SubmitReview propertyId={property.id} />}
+            <PropertyReviews propertyId={property.id} />
+
+        </section>
     )
 }
 
--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -4,6 +4,7 @@ import { FaRegHeart, FaHeart } from 'react-icons/fa';
 import { ReloadIcon } from '@radix-ui/react-icons';
 import { useFormStatus } from 'react-dom';
 import { Button } from '@/components/ui/button';
+import { LuTrash2, LuPenSquare } from 'react-icons/lu';
 type btnSize = 'default' | 'lg' | 'sm';
 
 type SubmitButtonProps = {
@@ -70,4 +71,24 @@ export const CardSubmitButton = ({ isFavorite }: { isFavorite: boolean }) => {
             )}
         </Button>
     );
+}
+
+type actionType = 'edit' | 'delete';
+
+export const IconButton = ({ actionType }: { actionType: actionType }) => {
+    const { pending } = useFormStatus();
+    const renderIcon = () => {
+        switch (actionType) {
+            case 'edit':
+                return <LuPenSquare />
+            case 'delete':
+                return <LuTrash2 />
+            default:
+                const never: never = actionType
+                throw new Error(`Invalid action type: ${never}`);
+        }
+    };
+    return <Button type="submit" size='icon' variant='link' className='p-2 cursor-pointer'>
+        {pending ? <ReloadIcon className='animate-spin' />: renderIcon()}
+    </Button>
 }
